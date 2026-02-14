3b5c4487e70ec99897e2121969da5912e372478c
Ilan Iwumbwe <ikibfm@gmail.com>
2026-02-14 16:57:44 +0000

    add docker run script

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -2,8 +2,5 @@
 
 IMAGE_NAME="qutefuzz:latest"
 
-echo "Building docker image"
-docker build -t $IMAGE_NAME .
-
 docker run -it --rm -v $(pwd)/nightly_results:/app/nightly_results $IMAGE_NAME \
     uv run scripts/run.py "$@"
